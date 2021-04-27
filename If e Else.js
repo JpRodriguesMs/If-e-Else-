54d4ad37b153ce207864bc3dbc8382e2be1a6ee7
JpRodriguesMs <80797986+JpRodriguesMs@users.noreply.github.com>
2021-04-27 19:02:44 -0300

    Add files via upload

---
+++ If e Else.js
@@ -0,0 +1,53 @@
+function calcular(){
+    var txtav1 = document.getElementById('txtav1');
+    var txtav2 = document.getElementById('txtav2');
+    var txtav3 = document.getElementById('txtav3');
+    var txtaluno = document.getElementById('txtaluno');
+    var aluno = (txtaluno.value)
+    var av1 = Number(txtav1.value)
+    var av2 = Number(txtav2.value)
+    var av3 = Number(txtav3.value)
+   
+    outav1.textContent = `Av1 = ${av1}`
+    outav2.textContent = `Av2 = ${av2}`
+    outav3.textContent = `Av3 = ${av3}`
+
+
+    if (av1 <= 4 || av2 <= 4) {
+        av2 = 0
+    }
+    if (av1 <= 4 || av2 <= 4) {
+        av1 = 0
+    }
+
+    if (av1 >= av2) {
+        if (av2 >= av3) {
+            av2 = av2
+        }
+        else {av2 = av3}
+    }
+    else if (av2 >= av1) {
+        if (av1 >= av3 ) {
+            av1 = av1
+        }
+        else{
+            av1 = av3
+        }     
+    }
+
+    if (av1 >= av2) {
+        if (av2 >= av3) {
+            av2 = av2
+        }
+        else {av2 = av3}
+    }
+
+    var media = (av1 + av2) / 2
+   
+    if (av1 < 4 || media < 6 || av2 < 4 ) { 
+        outresult.textContent= `Você ${aluno}, foi reprovado.`
+    }
+    else{
+        outresult.textContent= `Você ${aluno}, foi aprovado com a média  ${media} `
+    }
+} 
